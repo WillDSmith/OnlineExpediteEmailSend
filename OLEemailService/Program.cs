8c192c75ef947bb234cacf45387ad4707cad1904
Shakey <williedevonsmith@gmail.com>
2016-03-17 17:10:57 -0500

    Initial Commit

---
+++ OLEemailService/Program.cs
@@ -0,0 +1,118 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web;
+using Microsoft.Exchange.WebServices.Data;
+using System.Configuration;
+
+namespace OLEemailService
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ssReports report = new ssReports();
+            InternalEntities db = new InternalEntities();
+            
+            //string dtback = DateTime.Now.ToString("yyyy-MM-dd");
+            //DateTime date = Convert.ToDateTime(dtback);
+
+            DateTime dtt = DateTime.Now;
+            dtt = dtt.AddHours(-12);
+            
+            string dt = DateTime.Now.ToString("MMddyyyyhhmmss");
+            string path = @"ExportedFiles\";
+            string filename = "OLER" + dt + ".xlsx";
+            string log = @"Logs\";                                                             
+            string logfilename = dt + ".txt";
+
+            if (!Directory.Exists(log))
+            {
+                Directory.CreateDirectory(log);
+            }
+
+            FileStream filestream = new FileStream(log + logfilename, FileMode.Create);
+            var streamwriter = new StreamWriter(filestream);
+            streamwriter.AutoFlush = true;
+            Console.SetOut(streamwriter);
+            Console.SetError(streamwriter);
+
+            try
+            {
+                if (!Directory.Exists(path))
+                {
+                    Directory.CreateDirectory(path);
+                }
+                report.CreateExcelDoc(path + filename);
+
+                streamwriter.WriteLine("Excel file has been created!");
+            }
+
+            catch (Exception ex)
+            {
+                streamwriter.WriteLine(ex.ToString());
+            }
+            
+            try
+            {
+                String[] mEmailTo = ConfigurationManager.AppSettings["EmailTo"].ToString().Split(',');
+                string mEmailFrom = ConfigurationManager.AppSettings["EmailFrom"];
+                string mEmailSubject = ConfigurationManager.AppSettings["EmailSubject"];
+                
+                ExchangeService service = new ExchangeService(ExchangeVersion.Exchange2007_SP1);
+                service.UseDefaultCredentials = true;
+                service.AutodiscoverUrl(mEmailFrom, RedirectionUrlValidationCallback);
+
+                EmailMessage email = new EmailMessage(service);
+                foreach(String s in mEmailTo)
+                {
+                    email.ToRecipients.Add(s);
+                }
+                
+                email.Subject = mEmailSubject;
+                email.Body = new MessageBody("This email contains attachments");
+                email.Attachments.AddFileAttachment(path + filename);
+
+                streamwriter.WriteLine("Sending email...");
+                email.Send();
+                streamwriter.WriteLine("Email Sent....!");
+
+                var entries = db.OnlineExpedites.Where(d => d.CreationDate > dtt || d.DateSentTimeStamp == null);
+                if (entries != null)
+                {
+                    foreach (var entry in entries)
+                    {
+                        entry.DateSentTimeStamp = DateTime.Now;
+                    }
+                    db.SaveChanges();
+                }
+               
+                streamwriter.WriteLine("Database Updated.....");
+            }
+            catch (Exception ex)
+            {
+                streamwriter.WriteLine(ex.ToString());
+            }
+        }
+
+        private static bool RedirectionUrlValidationCallback(string redirectionUrl)
+        {
+            // The default for the validation callback is to reject the URL.
+            bool result = false;
+
+            Uri redirectionUri = new Uri(redirectionUrl);
+
+            // Validate the contents of the redirection URL. In this simple validation
+            // callback, the redirection URL is considered valid if it is using HTTPS
+            // to encrypt the authentication credentials. 
+            if (redirectionUri.Scheme == "https")
+            {
+                result = true;
+            }
+            return result;
+        }
+    }
+}
---
+++ OLEemailService/ssReports.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DocumentFormat.OpenXml;
+using DocumentFormat.OpenXml.Packaging;
+using DocumentFormat.OpenXml.Spreadsheet;
+using System.Data.Entity.SqlServer;
+
+namespace OLEemailService
+{
+    class ssReports
+    {
+        InternalEntities db = new InternalEntities();
+        
+        public void CreateExcelDoc(string fileName)
+        {
+            DateTime dtt = DateTime.Now;
+            dtt = dtt.AddHours(-12);
+            
+            using (SpreadsheetDocument document = SpreadsheetDocument.Create(fileName, SpreadsheetDocumentType.Workbook))
+            {
+                // Add a WorkbookPart to the document.
+                WorkbookPart workbookPart = document.AddWorkbookPart();
+                workbookPart.Workbook = new Workbook();
+
+                // Add a WorksheetPart to the WorkbookPart.
+                WorksheetPart worksheetPart = workbookPart.AddNewPart<WorksheetPart>();
+                worksheetPart.Worksheet = new Worksheet();
+                
+                Sheets sheets = workbookPart.Workbook.AppendChild(new Sheets());
+
+                Sheet sheet = new Sheet()
+                {
+                    Id = workbookPart.GetIdOfPart(worksheetPart),
+                    SheetId = 1,
+                    Name = String.Format("{0:MMddyyyyhhmmss}", DateTime.Now)
+                    
+                };
+
+                sheets.Append(sheet);
+
+                workbookPart.Workbook.Save();
+
+                var entries = db.OnlineExpedites.Where(d => d.CreationDate > dtt || d.DateSentTimeStamp == null);
+
+                if (!entries.Any())
+                    Environment.Exit(0);
+
+                SheetData sheetData = worksheetPart.Worksheet.AppendChild(new SheetData());
+
+                // Construct Header
+                Row row = new Row();
+
+                row.Append(
+                    ConstructCell("Request Id", CellValues.String),
+                    ConstructCell("EDP Tool Number", CellValues.String),
+                    ConstructCell("Date", CellValues.String),
+                    ConstructCell("Purchase Order", CellValues.String),
+                    ConstructCell("Item", CellValues.String),
+                    ConstructCell("Quantity", CellValues.String),
+                    ConstructCell("Message", CellValues.String),
+                    ConstructCell("By", CellValues.String),
+                    ConstructCell("Response", CellValues.String),
+                    ConstructCell("Germany Responder", CellValues.String));
+                    
+
+                // Insert the header row to the Sheet data
+                sheetData.AppendChild(row);
+
+                // Inserting each Entry
+                foreach (var entry in entries)
+                {
+                    row = new Row();
+                    row.Append(
+                        ConstructCell(entry.RequestId.ToString(), CellValues.Number),
+                        ConstructCell(entry.EDPToolNumber, CellValues.String),
+                        ConstructCell(entry.CreationDate.ToString(), CellValues.String),
+                        ConstructCell(entry.PurchaseOrderToGermany, CellValues.String),
+                        ConstructCell(entry.LineNumber.ToString(), CellValues.Number),
+                        ConstructCell(entry.QuantityRequested.ToString(), CellValues.Number),
+                        ConstructCell(entry.Message, CellValues.String),
+                        ConstructCell(entry.Name, CellValues.String),
+                        ConstructCell(entry.Response, CellValues.String),
+                        ConstructCell(entry.GermanyResponder, CellValues.String));
+                    sheetData.AppendChild(row);
+                }
+
+                worksheetPart.Worksheet.Save();
+                
+            }
+        }
+
+        private Cell ConstructCell(string value, CellValues dataType)
+        {
+            return new Cell()
+            {
+                CellValue = new CellValue(value),
+                DataType = new EnumValue<CellValues>(dataType)
+            };
+        }
+        
+    }
+}
